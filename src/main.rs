f29a19d60793afea7610c641fa40a562d41d81fe
rikoeldon <106416799+rikoeldon@users.noreply.github.com>
2023-11-27 22:32:12 -0600

    testing phase

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,8 @@ use crate::client::jsonrpc_client::JSONRPCClient;
 
 
 async fn fetch_headers() -> Result<(), Box<dyn std::error::Error>> {
-    let id = "2qMoscnJNq7h9XkLzWBGdFmvSMhnctXfHbiifQSfNN7shyA8SR";
-    let url_new = "http://127.0.0.1:41887/ext/bc/2qMoscnJNq7h9XkLzWBGdFmvSMhnctXfHbiifQSfNN7shyA8SR";
+    let id = "2WDSDR3iurcURidNEThTP1znebDfKTFBseCxUytqSzL7VD3huC";
+    let url_new = "http://127.0.0.1:56563/ext/bc/2WDSDR3iurcURidNEThTP1znebDfKTFBseCxUytqSzL7VD3huC";
     let cli = JSONRPCClient::new(url_new, 1337, id.to_string());
     let start = SystemTime::now().duration_since(UNIX_EPOCH)?.as_secs() as i64 * 1000;
     let end = start - 120 * 1000;
